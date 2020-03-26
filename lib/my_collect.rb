89165fd23414a527a121ee5d4177a323bac9a155
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 00:43:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
   i = 0
-  new_collection = []
+  new_array = []
   while i < array.length
-    new_collection << yield(array[i])
+    new_array << yield(array[i])
     i += 1
   end
-  new_collection
+  new_array
 end
